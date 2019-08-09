78130f2bfa4f98edeb38c8a9b80e996200ea1fea
CameronJWright <53836742+CameronJWright@users.noreply.github.com>
2019-08-09 14:14:27 -0400

    Add files via upload

---
+++ ImplicitPromotion.java
@@ -0,0 +1,12 @@
+package hello.world;
+
+public class ImplicitPromotion {
+
+	public static void main(String[] args) {
+		int a = 80;
+		//implicit type promotion
+		double b = a;
+		System.out.println(b);
+	}
+
+}
